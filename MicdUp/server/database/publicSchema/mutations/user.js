8fc7b6b838f2a0c89fef2e65f23ce7560ee353f2
Marty Rudolf <54113035+marty-rudolf@users.noreply.github.com>
2022-01-04 12:55:18 -0700

    Deleted posts with delete account.

--- MicdUp/server/database/publicSchema/mutations/user.js
+++ MicdUp/server/database/publicSchema/mutations/user.js
@@ -1,5 +1,7 @@
+const mongoose = require("mongoose");
 const { User } = require("../../models/User");
 const { Profile } = require("../../models/Profile");
+const { Post } = require("../../models/Post");
 const { GraphQLString } = require("graphql");
 const { MessageType } = require("../../types");
 const { sendEmail } = require("../../../utils/sendEmail");
@@ -45,17 +47,49 @@ const deleteAccount = {
   args: {},
   async resolve(parent, {}, context) {
     // FIXME: implement transaction
-    context.profile.following.forEach(function (i) {
-      const index = following.indexOf(context.profile.following[i]);
-      if (index > -1) {
-        following.splice(index, 1);
-      }
-    });
-    context.profile.followers.forEach(function (i) {});
-    let returnObject = {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    returnObject = {
       success: true,
       message: "Delete account successfully",
     };
+    try {
+      // For each person that you are following, unfollow them
+      for (let i = 0; i < context.profile.following.length; i++) {
+        const followingProfile = Profile.findById(context.profile.following[i]);
+        const index = followingProfile.followers.findIndex((id) => {
+          return id.toString() === context.profile.id;
+        });
+        // const index = followingProfile.followers[context.profile.id];
+        if (index > -1) {
+          followingProfile.followers.splice(index, 1);
+          await followingProfile.save({ session });
+        }
+      }
+
+      //For each of your followers, have them unfollow you
+      for (let i = 0; i < context.profile.followers.length; i++) {
+        const followerProfile = Profile.findById(context.profile.followers[i]);
+        const index = followingProfile.followers.findIndex((id) => {
+          return id.toString() === context.profile.id;
+        });
+        if (index > -1) {
+          followerProfile.following.splice(index, 1);
+          await followerProfile.save({ session });
+        }
+      }
+      // TODO: Force user to confirm delete account.
+      await Post.findByIdAndDelete(context.profile.id);
+      await User.findByIdAndDelete(context.user.id);
+      await Profile.findByIdAndDelete(context.profile.id);
+      await session.commitTransaction();
+    } catch (err) {
+      returnObject.success = false;
+      returnObject.message = err.message;
+      await session.abortTransaction();
+    } finally {
+      session.endSession();
+    }
     return returnObject;
   },
 };
