1ee4ce86762215edcfb40fe06715b4b855c04c94
OwenStadlwieser <47876306+OwenStadlwieser@users.noreply.github.com>
2022-02-09 14:10:15 -0700

    .

--- MicdUp/client/components/reuseable/ImagePickerModal.js
+++ MicdUp/client/components/reuseable/ImagePickerModal.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 // components
 import Modal from "react-native-modal";
-import { View, TouchableOpacity } from "react-native";
+import { View, TouchableOpacity, TouchableWithoutFeedback } from "react-native";
 // icons
 import { Entypo } from "@expo/vector-icons";
 import { AntDesign } from "@expo/vector-icons";
